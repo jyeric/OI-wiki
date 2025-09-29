006d98748e9e77d5e970b4164ce9e09c77371f30
jyeric <jyeric1@126.com>
2025-09-29 19:00:05 +0800

    refactor(archiveNewLink.py): Adding new arguments

--- archiveLink/archiveNewLink.py
+++ archiveLink/archiveNewLink.py
@@ -4,6 +4,8 @@ from Service.ArchiveOrg.history import latest_snapshot
 from Service.ArchiveOrg.create import save
 from blacklist import inBlacklist
 from redirect import finalRedirect
+from shortLink import shortLink
+from scanOriginalLink import detectURL
 
 def readExistingFile(filename):
     with open(filename, 'r', encoding='utf-8') as f:
@@ -20,6 +22,51 @@ def getLatestFile(path):
 def getArchiveLink(link):
     archiveLink = latest_snapshot(link)
     return archiveLink
+
+def newLink(key, value):
+    """
+    key: the link
+    value: a dict that have: file
+    return value that must have: file, status, failTime, fail, archiveLink, shortArchiveLink
+    """
+    # deal with status, failTime, fail
+    status = detectURL(key)
+    value["status"] = status
+    if status == 0:
+        value["failTime"] = 1
+    else:
+        value["failTime"] = 0
+    value["fail"] = False
+
+    if inBlacklist(key) == True:
+        # deal with archiveLink, shortArchiveLink
+        value["archiveLink"] = None
+        value["shortArchiveLink"] = None
+        return value
+    
+    # deal with archiveLink, shortArchiveLink
+    redirectUrl = finalRedirect(key)
+    a = latest_snapshot(key)
+
+    print(f"redirectUrl: {redirectUrl}")
+    print(f"key: {key}")
+    if redirectUrl != key:
+        # web.archive.org saves the content in the redirectUrl
+        b = latest_snapshot(redirectUrl)
+    if a != None:
+        value["archiveLink"] = a
+    elif b != None:
+        value["archiveLink"] = b
+    else:
+        # 该链接从未被存档
+        print(f"Saving: {key}")
+        save(key)
+        value["archiveLink"] = None
+        value["shortArchiveLink"] = None
+
+    if value["archiveLink"] != None:
+        value["shortArchiveLink"] = shortLink(value["archiveLink"])
+    return value
 
 def retryUnarchiveLink(data):
     """Some links has been archived for the first time 
@@ -55,25 +102,7 @@ def main():
         elif key.startswith("http"):
             # 处理新增的链接
             print(f"New link: {key}")
-            updateContent[key] = value
-            if inBlacklist(key) == True:
-                updateContent[key]["archiveLink"] = None
-                continue
-            redirectUrl = finalRedirect(key)
-            a = latest_snapshot(key)
-            print(f"redirectUrl: {redirectUrl}")
-            print(f"key: {key}")
-            if redirectUrl != key:
-                b = latest_snapshot(redirectUrl)
-            if a != []:
-                updateContent[key]["archiveLink"] = a
-            elif b != []:
-                updateContent[key]["archiveLink"] = b
-            else:
-                # 该链接从未被存档
-                print(f"Saving: {key}")
-                save(key)
-                updateContent[key]["archiveLink"] = None
+            updateContent[key] = newLink(key, value)
                 
     j = json.dumps(updateContent, indent=4, ensure_ascii=False)
     with open("archiveLink/data/data.json", "w", encoding="utf-8") as f:
@@ -81,4 +110,5 @@ def main():
     # print(j)
 
 if __name__ == "__main__":
-    main()
+    #main()
+    pass
